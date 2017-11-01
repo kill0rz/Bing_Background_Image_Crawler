726e96ac2551718edf195ca53ad4fa28066f401a
kill0rz <kill0rz@users.noreply.github.com>
2017-11-01 09:30:12 +0100

    fixed error: bing not reachable

--- html/crawler.php
+++ html/crawler.php
@@ -94,20 +94,22 @@ $site = file("http://bing.com/");
 $s = implode(" ", $site);
 $s = str_replace("\/", "/", $s);
 preg_match_all("/g_img=\{url: \"\/az\/hprichbg\/rb\/[a-zA-Z0-9_-]*\.jpg/", $s, $matches, PREG_OFFSET_CAPTURE);
-$filename_withpath = str_replace("g_img={url: \"", "", $matches[0][0][0]);
-$filename = preg_replace("/\/[a-zA-Z]*\/[a-zA-Z]*\/[a-zA-Z]*\//", "", $filename_withpath);
-$filename = str_replace("g_img={url: \"", "", $filename);
-$url = "http://www.bing.com" . $filename_withpath;
+if (isset($matches[0][0][0])) {
+	$filename_withpath = str_replace("g_img={url: \"", "", $matches[0][0][0]);
+	$filename = preg_replace("/\/[a-zA-Z]*\/[a-zA-Z]*\/[a-zA-Z]*\//", "", $filename_withpath);
+	$filename = str_replace("g_img={url: \"", "", $filename);
+	$url = "http://www.bing.com" . $filename_withpath;
 
-$dot = strrpos($filename, ".");
-$endung = substr($filename, $dot);
+	$dot = strrpos($filename, ".");
+	$endung = substr($filename, $dot);
 
-if (!file_exists("./pics/" . date("Y_m_d_") . $filename) && !file_exists("./pics/" . date('Y_m_d_', time() - 86400) . $filename)) {
-	if (trim($filename) == "") {
-		echo "Error: Empty filename!\n";
-	} elseif (!copy($url, "./pics/" . date("Y_m_d_") . $filename)) {
-		echo "Error while copying {$url} to ./pics/" . date("Y_m_d_") . $filename . "!\n";
-	} elseif (!resizeImage("./pics/" . date("Y_m_d_") . $filename, "./prevs/" . date("Y_m_d_") . preg_replace("/\.[a-z]{3}/", "", $filename) . "_preview" . $endung, 150)) {
-		echo "Fehler 2!\n";
+	if (!file_exists("./pics/" . date("Y_m_d_") . $filename) && !file_exists("./pics/" . date('Y_m_d_', time() - 86400) . $filename)) {
+		if (trim($filename) == "") {
+			echo "Error: Empty filename!\n";
+		} elseif (!copy($url, "./pics/" . date("Y_m_d_") . $filename)) {
+			echo "Error while copying {$url} to ./pics/" . date("Y_m_d_") . $filename . "!\n";
+		} elseif (!resizeImage("./pics/" . date("Y_m_d_") . $filename, "./prevs/" . date("Y_m_d_") . preg_replace("/\.[a-z]{3}/", "", $filename) . "_preview" . $endung, 150)) {
+			echo "Fehler 2!\n";
+		}
 	}
 }
